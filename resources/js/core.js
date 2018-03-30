e37f5c07e1f8294b9368c09a79afcff87d4d49f3
Ferph06 <fernando.perez060198@gmail.com>
2018-03-30 04:32:52 -0600

    se agregan imagenes de prueba,creacion de los formularios de login y registro,pre blog

--- resources/js/core.js
+++ resources/js/core.js
@@ -2,8 +2,11 @@
 M.AutoInit();
 const side = document.querySelector('.sidenav');
 const   instanceside = M.Sidenav.init(side,{});
- var elem = document.querySelector('.fixed-action-btn');
-var instance = new M.FloatingActionButton(elem, {
+if(document.querySelector('.fixed-action-btn')!==undefined && document.querySelector('.fixed-action-btn')!==null){
+    var elem = document.querySelector('.fixed-action-btn');
+    var instance = new M.FloatingActionButton(elem, {
     toolbarEnabled: true
   });
+}
+
 
