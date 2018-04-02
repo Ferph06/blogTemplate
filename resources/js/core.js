e1e93953241209ad7b6a5e286e11fbaa4876bcf4
Ferph06 <fernando.perez060198@gmail.com>
2018-04-02 03:50:56 -0500

    se agregan fotos y el estilo del blog

--- resources/js/core.js
+++ resources/js/core.js
@@ -1,7 +1,7 @@
 'use strict';
 M.AutoInit();
-const side = document.querySelector('.sidenav');
-const   instanceside = M.Sidenav.init(side,{});
+const side=document.querySelector('.sidenav');
+const   instanceside=M.Sidenav.init(side,{});
 if(document.querySelector('.fixed-action-btn')!==undefined && document.querySelector('.fixed-action-btn')!==null){
     var elem = document.querySelector('.fixed-action-btn');
     var instance = new M.FloatingActionButton(elem, {
@@ -10,3 +10,28 @@ if(document.querySelector('.fixed-action-btn')!==undefined && document.querySele
 }
 
 
+document.addEventListener("DOMContentLoaded",function(){
+    let barraLateral=document.querySelector('.blog-barra-lateral');
+    if(barraLateral!==null && barraLateral!==undefined){
+        barraLateral.setAttribute("style",`height:${document.body.offsetHeight}px !important`);
+    }
+});
+
+window.onscroll =function(){
+    if(document.querySelector('.blog-barra-lateral')!==null && document.querySelector('.blog-barra-lateral')!==undefined){
+        console.log("1"+window.pageYOffset);
+        console.log("2"+window.document.querySelector('.blog-barra-lateral').offsetTop);
+        if(window.pageYOffset>10){
+            document.querySelector('.blog-barra-lateral').classList.add("blog-barra-lateral-fix");
+            document.querySelector('#blog-content').classList.add("offset-s2");
+             document.querySelector('#blog-content').classList.add("offset-m2");
+            document.querySelector('#blog-content').classList.add("offset-l2");
+           }else{
+                document.querySelector('.blog-barra-lateral').classList.remove("blog-barra-lateral-fix");
+            document.querySelector('#blog-content').classList.remove("offset-s2");
+             document.querySelector('#blog-content').classList.remove("offset-m2");
+            document.querySelector('#blog-content').classList.remove("offset-l2");
+           }
+       
+    }
+}
